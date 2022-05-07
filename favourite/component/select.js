e9f64e6103b6b6c3f9e5c659de5c82b68b1ef955
PuneetChaubey <puneeteklavya@gmail.com>
2022-05-07 18:57:38 +0530

    updated cart

--- favourite/component/select.js
+++ favourite/component/select.js
@@ -2,17 +2,18 @@ let select  = ()=>{
     return `
     <select id="sel">
   
-   <option value="1">1</option>
-   <option value="2">2</option>
-   <option value="3">3</option>
-   <option value="4">4</option>
-   <option value="5">5</option>
-   <option value="6">6</option>
-   <option value="7">7</option>
-   <option value="8">8</option>
-   <option value="9">9</option>
-
-</select> 
+    <option value="1">1</option>
+    <option value="2">2</option>
+    <option value="3">3</option>
+    <option value="4">4</option>
+    <option value="5">5</option>
+    <option value="6">6</option>
+    <option value="7">7</option>
+    <option value="8">8</option>
+    <option value="9">9</option>
+ 
+ </select> 
+    
     `
 }
 export default select;
