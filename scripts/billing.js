21a0fe043808d99b2b00e4ef9e14ecf37acad7ca
PuneetChaubey <puneeteklavya@gmail.com>
2022-05-09 10:31:46 +0530

    address details added

--- scripts/billing.js
+++ scripts/billing.js
@@ -1,6 +1,9 @@
 let read = (a)=>{
     return document.getElementById(a).value;
 }
+let create = (b)=>{
+    return document.createElement(b)
+}
  let shippingAddress = ()=>{
    addressDetails = {
        name:read("fname") +" "+read("lname"),
@@ -49,3 +52,41 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 };
+
+
+let getAddress = JSON.parse(localStorage.getItem("address"))
+console.log(getAddress)
+ let addressbox = create("div")
+ 
+let name =create("p");
+let number = create("p")
+let street = create("p")
+let hnumber = create("p");
+let pincode = create("p");
+let city = create("p");
+name.innerText = getAddress.name;
+number.innerText = getAddress.pnumber
+street.innerText =getAddress.street;
+pincode.innerText = getAddress.postalcode;
+ city.innerText = getAddress.city+", " + getAddress.state;
+console.log(name,number,street,pincode,city)
+console.log(pincode)
+addressbox.setAttribute("class","addressdetails")
+addressbox.append(name,number,street,pincode,city)
+console.log(addressbox,"djwidj")
+document.querySelector("#address").append(addressbox)
+let deliveryaddress =create("div")
+let name1 =create("p");
+let number1 = create("p")
+let street1 = create("p")
+let hnumber1 = create("p");
+let pincode1 = create("p");
+let city1 = create("p");
+deliveryaddress.setAttribute("class","addressdetails")
+name1.innerText = getAddress.name;
+number1.innerText = getAddress.pnumber
+street1.innerText =getAddress.street;
+pincode1.innerText = getAddress.postalcode;
+ city1.innerText = getAddress.city+", " + getAddress.state;
+deliveryaddress.append(name1,number1,street1,pincode1,city1)
+document.querySelector("#address1").append(deliveryaddress)
